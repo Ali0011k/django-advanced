28613d2b8ff40809c0e4643f93d7d759545496de
ali <kly441781@gmail.com>
2023-11-02 15:43:03 +0330

    permission changed

--- accounts/api/v1/permissions.py
+++ accounts/api/v1/permissions.py
@@ -2,16 +2,13 @@ from rest_framework import permissions
 
 
 class IsAdminOrOwner(permissions.BasePermission):
-    
-    def has_permission(self, request, view):
-        """ checking user is superuser/staff or not """
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return bool(request.user.is_superuser or request.user.is_staff)
-    
+    """ a permission for checking user is owner/admin or not """
     
     def has_object_permission(self, request, view, obj):
-        """ checking user is owner or not """
         if request.method in permissions.SAFE_METHODS:
             return True
+        
+        if request.user.is_superuser or request.user.is_staff:
+            return True
+        
         return bool(request.user.id == obj.id)
